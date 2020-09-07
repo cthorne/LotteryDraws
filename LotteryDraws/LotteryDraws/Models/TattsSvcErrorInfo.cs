ae82c5bfb3c8c4f18a1b10a54fe4987e65e3cf6a
Chris <christhorn3@gmail.com>
2020-09-07 18:57:39 +1000

    Models for OpenlotteriesDraw request
    Enums and models for request

---
+++ LotteryDraws/LotteryDraws/Models/TattsSvcErrorInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace LotteryDraws.Models
+{
+    public class TattsSvcErrorInfo
+    {
+        public int SystemId { get; set; }
+        public int ErrorNo { get; set; }
+        public string DisplayMessages { get; set; }
+        public bool ContactSupport { get; set; }
+        public string SupportErrorReference { get; set; }
+    }
+}
