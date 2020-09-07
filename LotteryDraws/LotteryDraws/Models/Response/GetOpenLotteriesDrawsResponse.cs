5f595a3f575d0362fc43abbef881c95ff2d813e9
Chris <christhorn3@gmail.com>
2020-09-07 18:58:34 +1000

    Response object for GetOpenLotteriesDraws

---
+++ LotteryDraws/LotteryDraws/Models/Response/GetOpenLotteriesDrawsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace LotteryDraws.Models.Response
+{
+    public class GetOpenLotteriesDrawsResponse
+    {
+        public IEnumerable<OpenLotteriesDraw> OpenLotteriesDraws { get; set; }
+        public TattsSvcErrorInfo ErrorInfo { get; set; }
+    }
+}
