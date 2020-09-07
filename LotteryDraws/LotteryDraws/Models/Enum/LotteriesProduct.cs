b7cbe75ea825212cf75401151f0917bc3776a3bf
Chris <christhorn3@gmail.com>
2020-09-07 18:40:15 +1000

    Add enums for GetOpenLotteriesDraws

---
+++ LotteryDraws/LotteryDraws/Models/Enum/LotteriesProduct.cs
@@ -0,0 +1,27 @@
+﻿namespace LotteryDraws.Models.Enum
+{
+    /// <summary>
+    /// Lottery products.
+    /// </summary>
+    public enum LotteriesProduct
+    {
+        None,
+        TattsLotto,
+        OzLotto,
+        Powerball,
+        Super66,
+        Pools,
+        MonWedLotto,
+        LuckyLotteries2,
+        LuckyLotteries5,
+        LottoStrike,
+        WedLotto,
+        Keno,
+        CoinToss,
+        SetForLife,
+        MultiProduct = 15,
+        InstantScratchIts,
+        TwoDollarCasket,
+        BonusDraws
+    }
+}
