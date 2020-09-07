98d55aa3e30f37ca77d6953785fbc8ce9832c4bc
Chris <christhorn3@gmail.com>
2020-09-07 20:23:34 +1000

    Initial HTTPClient class
    Requires more work or rework

---
+++ LotteryDraws/LotteryDraws/Services/HttpClientHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LotteryDraws.Models.Request;
+using LotteryDraws.Models.Response;
+
+namespace LotteryDraws.Services
+{
+    public class HttpClientHelper : IHttpClientHelper
+    {
+
+        public Task<GetOpenLotteriesDrawsResponse> GetOpenLotteriesDrawsRequestTask(GetOpenLotteriesDrawsRequest request)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LotteryDraws/LotteryDraws/Services/IHttpClientHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using LotteryDraws.Models.Request;
+using LotteryDraws.Models.Response;
+
+namespace LotteryDraws.Services
+{
+    interface IHttpClientHelper
+    {
+        Task<GetOpenLotteriesDrawsResponse> GetOpenLotteriesDrawsRequestTask(GetOpenLotteriesDrawsRequest request);
+    }
+}
