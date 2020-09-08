ceb85c044e09d7e3413417f7371499c96f94a2ff
Chris <christhorn3@gmail.com>
2020-09-08 17:44:26 +1000

    Prototyping connection to UI

--- LotteryDraws/LotteryDraws/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ LotteryDraws/LotteryDraws/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -7,10 +7,11 @@ import { HttpClient } from '@angular/common/http';
 })
 export class FetchDataComponent {
   public forecasts: WeatherForecast[];
+  public test: OpenLotteriesDraw[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
+    http.get<GetOpenLotteriesDrawsResponse>(baseUrl + 'weatherforecast').subscribe(result => {
+      this.test = result.openLotteriesDraws;
     }, error => console.error(error));
   }
 }
@@ -20,4 +21,86 @@ interface WeatherForecast {
   temperatureC: number;
   temperatureF: number;
   summary: string;
+}
+
+
+enum DrawTypes {
+  Undefined = "Undefined",
+  BaseWeek = "BaseWeek",
+  Jackpot = "Jackpot",
+  Superdraw = "Superdraw",
+  Megadraw = "Megadraw",
+  DoubleDivs = "DoubleDivs",
+  TripleDivs = "TripleDivs",
+  FourTimesDivs = "FourTimesDivs",
+  DoubleDivsPlus1 = "DoubleDivsPlus1",
+  TripleDivsPlus1 = "TripleDivsPlus1",
+  FourTimesDivsPlus1 = "FourTimesDivsPlus1",
+  CashCadeNextDiv = "CashCadeNextDiv",
+  CashCadeAllDivs = "CashCadeAllDivs"
+}
+
+enum LotteriesProduct {
+  None = "None",
+  TattsLotto = "TattsLotto",
+  OzLotto = "OzLotto",
+  Powerball = "Powerball",
+  Super66 = "Super66",
+  Pools = "Pools",
+  MonWedLotto = "MonWedLotto",
+  LuckyLotteries2 = "LuckyLotteries2",
+  LuckyLotteries5 = "LuckyLotteries5",
+  LottoStrike = "LottoStrike",
+  WedLotto = "WedLotto",
+  Keno = "Keno",
+  CoinToss = "CoinToss",
+  SetForLife = "SetForLife744",
+  MultiProduct = "MultiProduct",
+  InstantScratchIts = "InstantScratchIts",
+  TwoDollarCasket = "TwoDollarCasket",
+  BonusDraws = "BonusDraws"
+}
+
+enum LotteriesCompany {
+  None = "None",
+        Tattersalls = "Tattersalls",
+        GoldenCasket = "GoldenCasket",
+        NSWLotteries = "NSWLotteries",
+        NTLotteries = "NTLotteries",
+        SALotteries = "SALotteries"
+}
+
+interface GetOpenLotteriesDrawsRequest {
+  CompanyId: LotteriesCompany,
+  MaxDrawCount: number,
+  OptionalProductFilter: string[]
+}
+
+interface OpenLotteriesDraw {
+  ProductId: LotteriesProduct,
+  DrawNumber: number,
+  DrawDisplayName: string,
+  DrawDate: Date,
+  DrawLogoUrl: string,
+  DrawType: DrawTypes,
+  Div1Amount: number,
+  IsDiv1Estimated: boolean,
+  IsDiv1Unknown: boolean,
+  DrawCloseDateTimeUTC: Date,
+  DrawEndSellDateTimeUTC: Date,
+  DrawCountDownTimerSeconds: number
+}
+
+interface TattsSvcErrorInfo {
+  SystemId: number,
+  ErrorNo: number,
+  DisplayMessage: string,
+  ContactSupport: boolean,
+  SupportErrorReference: string
+}
+
+interface GetOpenLotteriesDrawsResponse {
+  openLotteriesDraws: OpenLotteriesDraw[],
+  errorInfo: TattsSvcErrorInfo,
+  success: boolean
 }
