52693eeb23ad93c8937cad9b83bf4f4d382638bd
Chris <christhorn3@gmail.com>
2020-09-08 15:06:34 +1000

    Correct naming for error message field

--- LotteryDraws/LotteryDraws/Models/TattsSvcErrorInfo.cs
+++ LotteryDraws/LotteryDraws/Models/TattsSvcErrorInfo.cs
@@ -4,7 +4,7 @@
     {
         public int SystemId { get; set; }
         public int ErrorNo { get; set; }
-        public string DisplayMessages { get; set; }
+        public string DisplayMessage { get; set; }
         public bool ContactSupport { get; set; }
         public string SupportErrorReference { get; set; }
     }
